ee63aea6659f91aa67ae50ad4a5717460eaaa3d2
akaiferato <fernandoalagos@gmail.com>
2025-08-26 19:58:15 -0400

    commit 3 con actividad 1 realizada

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    context = {}
-    return render(request, "dispositivos/index.html")
+    dispositivos = [
+            {"nombre" : "Sensor Temperatura", "consumo" : 50},
+            {"nombre" : "Medidor Solar", "consumo" : 120},
+            {"nombre" : "Sensor Movimiento", "consumo" : 30},
+            {"nombre" : "Calefactor", "consumo" : 200}
+        ]
+    consumo_maximo = 100
+    context = {"dispositivos" : dispositivos,
+               "consumo_maximo" : consumo_maximo}
+
+    return render(request, "dispositivos/index.html", context)
