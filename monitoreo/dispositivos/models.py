b155f3d6561a0453804c53972ab57d0c0ab7fa6d
akaiferato <fernandoalagos@gmail.com>
2025-09-01 03:14:08 -0400

    Agregacion de modelos en rama-fernando

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True, null=True)
+class Medicion:
+    consumo = models.IntegerField()
+    fecha = models.DateField()
+    limite_consumo = models.IntegerField()
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
+    
+    def __str__():
+        return self.consumo
 
-    def __str__(self):
-        return self.nombre
+class Alerta:
+    nombre = models.CharField(max_length=20)
+    descripcion = models.TextField()
+    medicion = models.OneToOneField(Medicion, on_delete=models.CASCADE)
+
+    def __str__():
+        return nombre
 
 
