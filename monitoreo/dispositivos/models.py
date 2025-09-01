0f3a8bb6c499130673d4f2e03a00e5a1f43f8bca
akaiferato <fernandoalagos@gmail.com>
2025-09-01 01:42:31 -0400

    commit para empezar trabajo colaborativo

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -2,17 +2,9 @@ from django.db import models
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.Textfield(blank=True, null=True)
+    descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
 
-class Dispositivo(models.Model):
-    nombre = models.CharField(max_length=100)
-    consumo = models.IntegerField()
-    estado = models.BooleanField(default=True)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nombre
 
